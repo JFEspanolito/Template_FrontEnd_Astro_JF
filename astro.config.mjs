45b5cdebdc6c0f94be2fffcd2e0e1e5eda143f1d
JFEspanolito <software.jf.revisit192@passinbox.com>
2026-02-05 14:29:12 -0600

    add style, config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,30 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+// Integraciones
+import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+
+// Tailwind v4 (Vite Plugin)
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // ⚠️ CAMBIAR: Importante para que funcione el sitemap
+  site: 'https://tudominio.com', 
+
+  integrations: [
+    react(),
+    sitemap(),
+    partytown({
+      config: { forward: ['dataLayer.push'] },
+    }),
+  ],
+
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+});
